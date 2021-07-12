d5c4ea53af39719a9ff805d55d2802b7c93b1545
wahyua111 <85115384+wahyua111@users.noreply.github.com>
2021-07-12 09:26:19 +0700

    Update private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -59,7 +59,7 @@ Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
                         "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
                 ],[
                     InlineKeyboardButton(
-                        "🎁 Donate", url=f"https://t.me/VckyouuBitch")
+                        "🎁 Donate", url=f"https://t.me/FAM1LY")
                 ]
             ]
         ),
@@ -168,10 +168,10 @@ async def reload(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "Group Support", url=f"https://t.me/GeezSupportGroup"
+                        "Group Support", url=f"https://t.me/FAMILYNIRVANA"
                     ),
                     InlineKeyboardButton(
-                        "Created By", url=f"https://t.me/VckyouuBitch"
+                        "Created By", url=f"https://t.me/FAM1LY"
                     )
                 ]
             ]
